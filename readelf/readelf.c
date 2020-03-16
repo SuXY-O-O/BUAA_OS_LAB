47994d8ad53b525fec225ca9dd955e722343d6d2
SuXY <18373187@buaa.edu.cn>
2020-03-16 16:50:22 +0000

    finish lab1extra 1

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -49,12 +49,15 @@ int readelf(u_char *binary, int size)
 
         int Nr;
 
-        Elf32_Shdr *shdr = NULL;
+	int endingFlag = 0; // Chenk type: 0 small 1 big
 
-        u_char *ptr_sh_table = NULL;
-        Elf32_Half sh_entry_count = ehdr->e_shnum;
-        Elf32_Half sh_entry_size = ehdr->e_shentsize;
+	Elf32_Phdr *phdr = NULL;
 
+	Elf32_Half ph_entry_count = ehdr->e_phnum;
+
+	Elf32_Half ph_entry_size = ehdr->e_phentsize;
+
+	Elf32_Off ph_offset = ehdr->e_phoff;
 
         // check whether `binary` is a ELF file.
         if (size < 4 || !is_elf_format(binary)) {
@@ -62,10 +65,31 @@ int readelf(u_char *binary, int size)
                 return 0;
         }
 	else {
-		shdr = ((u_char *)ehdr + ehdr->e_shoff);
-		for (Nr = 0; Nr < sh_entry_count; Nr++) {
-			printf("%d:0x%x\n", Nr, shdr->sh_addr);
-			shdr = ((u_char *)shdr + sh_entry_size);
+		// Check big or small
+		if (*(binary + 5) ==  2) {
+			endingFlag = 1;
+			ph_entry_count = BSWAP_16(ph_entry_count);
+			ph_entry_size = BSWAP_16(ph_entry_size);
+			ph_offset = BSWAP_32(ph_offset);
+		}
+		else if (*(binary + 5) == 1) {
+			endingFlag = 0;
+		}
+		else {
+			printf("illegal formate\n");
+			return 0;
+		}
+		phdr = (Elf32_Phdr *)((u_char *)ehdr + ph_offset);
+		for (Nr = 0; Nr < ph_entry_count; Nr++) {
+			if (endingFlag) {
+				Elf32_Word fisz = BSWAP_32(phdr->p_filesz);
+				Elf32_Word mesz = BSWAP_32(phdr->p_memsz);
+				printf("%d:0x%x,0x%x\n", Nr, fisz, mesz);
+			}
+			else {
+				printf("%d:0x%x,0x%x\n", Nr, phdr->p_filesz, phdr->p_memsz);
+			}
+			phdr = (Elf32_Phdr *)((u_char *)phdr + ph_entry_size);
 		}
 	}
         // get section table addr, section header number and section header size.
