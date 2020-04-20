5cc3d55337ac28239a2c5694dcf790715fec72c9
SuXY <18373187@buaa.edu.cn>
2020-04-20 22:11:03 +0000

    pure lru in 16

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -3,35 +3,43 @@
 void pageReplace (long* physic_memery, long nwAdd)
 {
     long page = nwAdd >> 12;
-    unsigned int group = (page & 0b111) << 3;
+    unsigned int group = page & 0b11;
+    unsigned int index = group << 4;
+    unsigned int next_index = ((group + 1) % 4) << 4;
     int i;
     int static sign[64] = {0};
     for (i = 0; i < 64; i++)
         sign[i]++;
-    for (i = group; i < group + 8; i++)
+    for (i = 0; i < 16; i++)
         {
-            if (page == physic_memery[i])
+            if (page == physic_memery[index + i])
             {
-                sign[i] = 0;
+                sign[index + i] = 0;
                 return;
             }
         }
-    for (i = group; i < group + 8; i++)
+    for (i = 0; i < 16; i++)
     {
-        if (physic_memery[i] == 0)
+        if (physic_memery[i + index] == 0)
         {
-            physic_memery[i] = page;
-            sign[i] = 0;
+            physic_memery[i + index] = page;
+            sign[index + i] = 0;
+            physic_memery[i + next_index] = page + 1;
+            sign[next_index + i] = 0;
             return;
         }
     }
     int max = 0;
-    for (i = group; i < group + 8; i++)
+    int next_max = 0;
+    for (i = 0; i < 16; i++)
     {
-        if (sign[i] >= sign[max])
+        if (sign[index + i] > sign[index + max])
             max = i;
+        if (sign[next_index + i] > sign[next_index + next_max])
+            next_max = i;
     }
-    physic_memery[max] = page;
-    sign[max] = 0;
-    return;
+    physic_memery[max + index] = page;
+    physic_memery[next_max + next_index] = page + 1;
+    sign[index + max] = 0;
+    sign[next_index + next_max] = 0;
 }
