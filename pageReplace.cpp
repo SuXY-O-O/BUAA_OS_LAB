a4ede638f4f8f49e537cf3cdf0c62f41399ca4fa
SuXY <18373187@buaa.edu.cn>
2020-04-21 20:20:34 +0000

    8 with 1 for

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -4,31 +4,23 @@ void pageReplace (long* physic_memery, long nwAdd)
 {
     long page = nwAdd >> 12;
     unsigned int group = (page & 0b111) << 3;
-    int i;
+    unsigned int end = group + 8;
     int static sign[64] = {0};
-    //int static count[64] = {0};
-    for (i = 0; i < 64; i++)
+    for (int i = 0; i < 64; i++)
         sign[i]++;
-    for (i = group; i < group + 8; i++)
+    int max = 0;
+    for (int i = group; i < end; i++)
     {
         if (page == physic_memery[i])
         {
-            //count[i]++;
-            //if (count[i] == 2)
-                sign[i] = 0;
-            //else
-                //sign[i] -= 5;
+            sign[i] = 0;
             return;
         }
-    }
-    int max = 0;
-    for (i = group; i < group + 8; i++)
-    {
         if (sign[i] > sign[max])
             max = i;
     }
     physic_memery[max] = page;
     sign[max] = 0;
-    //count[max] = 1;
     return;
 }
+
