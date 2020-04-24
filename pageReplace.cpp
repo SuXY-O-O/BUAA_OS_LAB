3481cfaba16da9a63206073362e925d4c4f830fa
SuXY <18373187@buaa.edu.cn>
2020-04-24 17:08:42 +0000

    4 lru advance

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -3,26 +3,42 @@
 void pageReplace (long* physic_memery, long nwAdd)
 {
     long page = nwAdd >> 12;
-    unsigned int group = (page & 0b1111) << 2;
-    unsigned int end = group + 4;
-    int i;
+    unsigned int group0 = (page & 0b1111) << 2;
+    unsigned int group1 = group0 + 1;
+    unsigned int group2 = group0 + 2;
+    unsigned int group3 = group0 + 3;
     int static sign[64] = {0};
-    for (i = group; i < end; i++)
-        sign[i]++;
-    for (i = group; i < end; i++)
+    sign[group0]++;
+    sign[group1]++;
+    sign[group2]++;
+    sign[group3]++;
+    if (page == physic_memery[group0])
     {
-        if (page == physic_memery[i])
-        {
-            sign[i] = 0;
-            return;
-        }
+        sign[group0] = 0;
+        return;
     }
-    int max = 0;
-    for (i = group; i < end; i++)
+    if (page == physic_memery[group1])
     {
-        if (sign[i] > sign[max])
-            max = i;
+        sign[group1] = 0;
+        return;
     }
+    if (page == physic_memery[group2])
+    {
+        sign[group2] = 0;
+        return;
+    }
+    if (page == physic_memery[group3])
+    {
+        sign[group3] = 0;
+        return;
+    }
+    int max = group0;
+    if (sign[group1] > sign[max])
+        max = group1;
+    if (sign[group2] > sign[max])
+        max = group2;
+    if (sign[group3] > sign[max])
+        max = group3;
     physic_memery[max] = page;
     sign[max] = 0;
     return;
