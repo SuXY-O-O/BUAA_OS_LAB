9769e98bbe20951b181ad228aa6b8b7d70c567c2
SuXY <18373187@buaa.edu.cn>
2020-05-18 13:51:49 +0000

    advance 3 fix \n

--- lib/handle_cons_ir.c
+++ lib/handle_cons_ir.c
@@ -5,11 +5,11 @@ void handle_cons_ir(char c, int status)
 {
     static char data[64] = {0};
     static int count = 0;
-    printf("cp0 status: %x/n", status);
+    printf("cp0 status: %x\n", status);
     if (c == '\r')
     {
         data[count] = '\0';
-        count--;
+        //count--;
         printf("length: %d\n", count);
         printf("content: %s\n", data);
         count = 0;
